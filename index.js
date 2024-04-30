59f8e17ccd7c04cf6bc2f81a92b648a23f10074b
EMON <mamudmdemon@gmail.com>
2024-04-30 18:14:27 +0600

    update craft added without complete

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const cors = require('cors');
-const app=express();
-const port=process.env.port || 5000;
-
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 5000;
 
 //middleWare
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.2fsgp3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,30 +27,50 @@ async function run() {
 
     const craftCollection = client.db("craftDB").collection("craft");
 
+    app.get("/crafts", async (req, res) => {
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/crafts', async(req,res)=>{
-      const cursor=  craftCollection.find();
-      const result= await cursor.toArray()
-      res.send(result)
-    })
+    app.post("/crafts", async (req, res) => {
+      const newCraft = req.body;
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+    });
 
-
-    app.post('/crafts', async(req,res)=>{
-      const newCraft=req.body;
-      const result= await craftCollection.insertOne(newCraft);
-      res.send(result)
-    })
-
-    app.delete('/crafts/:id', async(req,res)=>{
-      const id=req.params.id;
+    app.put("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const craft = req.body;
+      const updateCraft = {
+        $set: {
+          itemName: craft.itemName,
+          subCategory: craft.subCategory,
+          price: craft.price,
+          customization: craft.customization,
+          description: craft.description,
+          image: craft.image,
+          processingTime: craft.processingTime,
+          rating: craft.rating,
+          stockStatus:craft.stockStatus
+        },
+      };
+      res.send(result);
+    });
+    app.delete("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result= await craftCollection.deleteOne(query);
-      res.send(result)
-    })
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -62,11 +78,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
